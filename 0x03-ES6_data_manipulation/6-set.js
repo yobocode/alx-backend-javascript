78ffea05d2b8815f07f140a459d5fa3947de42cb
yobocode <yibrahamare@gmail.com>
2023-10-03 13:28:54 +0300

    task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/*
+ * Creates a set from an array.
+ * @param {Array} arr - The source array.
+ * @author Yibrah Teare <https://github.com/yobocode>
+ * @returns {Set}
+ */
+
+export default function setFromArray(arr) {
+  return new Set(arr);
+}
