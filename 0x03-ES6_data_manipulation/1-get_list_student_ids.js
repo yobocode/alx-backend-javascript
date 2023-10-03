a3b19231acc0408811b345635b81f2684697d6e9
yobocode <yibrahamare@gmail.com>
2023-10-03 13:15:16 +0300

    task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrives ids from a list of students
+ * @param {{
+ * id: Number,
+ * firstName: String,
+ * location: String
+ * }[]} students - The list of students
+ * @author Bekalu Geta <https://github.com/yobocode>
+ * @returns An array of student ids
+ */
+export default function getListStudentsIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
